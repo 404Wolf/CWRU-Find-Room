046804e57a68ab1969f01560aaff083689360173
Wolf <caffeinate@msn.com>
2024-02-11 22:31:53 -0500

    Save auth to disc

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -155,6 +155,7 @@ def reauth(username: str, password: str):
             }
         ),
     )
+    cache.bgsave()
 
     logger.info("Auth refreshed")
     logger.debug(f"Auth: {json.loads(cache.get('auth'))}")
